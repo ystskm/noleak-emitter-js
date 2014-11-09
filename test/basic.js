6daa1de07fed4ef6dc626ba1bf4b2ed74a8c9f8f
ystskm <brilliantpenguin@gmail.com>
2014-11-09 22:50:40 +0900

    add test

--- test/basic.js
+++ test/basic.js
@@ -1,64 +1,44 @@
 var nodeunit = require('nodeunit');
-var jsdom = require("jsdom");
-
-var repo_be = 'https://raw.github.com/ystskm/browser-emitter-js/';
-var repo_ne = 'https://raw.github.com/ystskm/noleak-emitter-js/';
-
-var scripts = [];
-scripts.push(repo_be + "master/Emitter.js");
-scripts.push(repo_ne + "master/NoleakEmitter.js");
-
-function setup(callback) {
-  jsdom.env("<html><head></head><body></body></html>", {
-    scripts: scripts
-  }, function(errors, window) {
-    errors && console.error(errors), callback(window);
-  });
-}
+var NoleakEmitter = require('noleak-emitter');
 
 module.exports = nodeunit.testCase({
   'readme': function(t) {
-    setup(function(window) {
 
-      var TestEmitter = window.NoleakEmitter;
-      t.equal(typeof TestEmitter, 'function');
+    var emitter = new NoleakEmitter();
+    var cnt = 0;
 
-      var emitter = new TestEmitter();
-      var cnt = 0;
+    emitter.on('readme-test', function() {
+      t.equal(arguments[0], 0);
+      t.equal(arguments[1], 1);
+      t.equal(arguments[2], 2);
+      cnt++;
+    });
+    emitter.on('error', function() {
+      t.equal(arguments[0], 3);
+      t.equal(arguments[1], 4);
+      t.equal(arguments[2], 5);
+      cnt++;
+    });
+    emitter.on('end', function() {
+      t.equal(arguments[0], 3);
+      t.equal(arguments[1], 4);
+      t.equal(arguments[2], 5);
+      cnt++;
+    });
 
-      emitter.on('readme-test', function() {
-        t.equal(arguments[0], 0);
-        t.equal(arguments[1], 1);
-        t.equal(arguments[2], 2);
-        cnt++;
-      });
-      emitter.on('error', function() {
-        t.equal(arguments[0], 3);
-        t.equal(arguments[1], 4);
-        t.equal(arguments[2], 5);
-        cnt++;
-      });
-      emitter.on('end', function() {
-        t.equal(arguments[0], 3);
-        t.equal(arguments[1], 4);
-        t.equal(arguments[2], 5);
-        cnt++;
-      });
+    emitter.emit('readme-test', 0, 1, 2);
+    emitter.emit('end', 3, 4, 5);
 
+    setImmediate(function() {
       emitter.emit('readme-test', 0, 1, 2);
+      try {
+        emitter.emit('error');
+      } catch(e) {
+        t.ok(e.message); // should throw Unhandled 'error' event
+      }
       emitter.emit('end', 3, 4, 5);
-
-      setTimeout(function() {
-        emitter.emit('readme-test', 0, 1, 2);
-        try {
-          emitter.emit('error');
-        } catch(e){
-          t.ok(e.message); // should throw Unhandled 'error' event
-        }
-        emitter.emit('end', 3, 4, 5);
-        t.equal(cnt, 2);
-        t.done();
-      }, 4);
+      t.equal(cnt, 2);
+      t.done();
     });
   }
 });
