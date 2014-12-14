c4a4b5158201e6a55b9e092aeebc912768d8b570
ystskm <brilliantpenguin@gmail.com>
2014-12-15 08:35:25 +0900

    fix test

--- test/basic.js
+++ test/basic.js
@@ -5,25 +5,22 @@ module.exports = nodeunit.testCase({
   'readme': function(t) {
 
     var emitter = new NoleakEmitter();
-    var cnt = 0;
+    t.expect(7);
 
     emitter.on('readme-test', function() {
-      t.equal(arguments[0], 0);
-      t.equal(arguments[1], 1);
-      t.equal(arguments[2], 2);
-      cnt++;
+      t.equal(arguments[0], 0); // No.1
+      t.equal(arguments[1], 1); // No.2
+      t.equal(arguments[2], 2); // No.3
     });
     emitter.on('error', function() {
       t.equal(arguments[0], 3);
       t.equal(arguments[1], 4);
       t.equal(arguments[2], 5);
-      cnt++;
     });
     emitter.on('end', function() {
-      t.equal(arguments[0], 3);
-      t.equal(arguments[1], 4);
-      t.equal(arguments[2], 5);
-      cnt++;
+      t.equal(arguments[0], 3); // No.4
+      t.equal(arguments[1], 4); // No.5
+      t.equal(arguments[2], 5); // No.6
     });
 
     emitter.emit('readme-test', 0, 1, 2);
@@ -34,10 +31,10 @@ module.exports = nodeunit.testCase({
       try {
         emitter.emit('error');
       } catch(e) {
+        // No.7
         t.ok(e.message); // should throw Unhandled 'error' event
       }
       emitter.emit('end', 3, 4, 5);
-      t.equal(cnt, 2);
       t.done();
     });
   }
